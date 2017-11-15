151ab2e07c6ac5f2bf363b7535dcdaeced117556
Shanzhuo <shanzhuo.zhang@gmail.com>
2017-11-15 02:20:46 +0000

    Test menu creation.

--- src/main.py
+++ src/main.py
@@ -15,3 +15,41 @@ urls = (
 if __name__ == '__main__':
     app = web.application(urls, globals())
     app.run()
+
+    from wechatpy import WeChatClient
+
+    client = WeChatClient("wx45cfb9696e261322", "e4d25ececfb46b2366a299cdbf67c337")
+    client.menu.create({
+        "button": [
+            {
+                "type": "click",
+                "name": "今日歌曲",
+                "key": "V1001_TODAY_MUSIC"
+            },
+            {
+                "type": "click",
+                "name": "歌手简介",
+                "key": "V1001_TODAY_SINGER"
+            },
+            {
+                "name": "菜单",
+                "sub_button": [
+                    {
+                        "type": "view",
+                        "name": "搜索",
+                        "url": "http://www.soso.com/"
+                    },
+                    {
+                        "type": "view",
+                        "name": "视频",
+                        "url": "http://v.qq.com/"
+                    },
+                    {
+                        "type": "click",
+                        "name": "赞一下我们",
+                        "key": "V1001_GOOD"
+                    }
+                ]
+            }
+        ]
+    })
